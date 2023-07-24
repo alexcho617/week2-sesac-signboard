2d8141d4d4dd72d45a966b52b73d6a132cec3328
Alex Cho <38528052+alexcho617@users.noreply.github.com>
2023-07-24 22:46:28 +0900

    [Feat] UI & Random Color -가로와 세로모드에 맞게 UI를 AutoLayout으로 잡았습니다 - Timer를 이용하여 전광판의 색이 랜덤으로 계속 바뀌는 효과를 넣었습니다.

--- signboard/ViewController.swift
+++ signboard/ViewController.swift
@@ -8,12 +8,113 @@
 import UIKit
 
 class ViewController: UIViewController {
-
+    
+    //variables and outlets
+    @IBOutlet var textField: UITextField!
+    @IBOutlet var scrollView: UIScrollView!
+    @IBOutlet var labelView: UILabel!
+    @IBOutlet var sendButton: UIButton!
+    @IBOutlet var randomButton: UIButton!
+    @IBOutlet var editButton: UIButton!
+    
+    @IBOutlet var cycleButton: UIButton!
+    var timeTrigger = false
+    var realTime = Timer()
+    var isColorOnRepeat = false
+    
+    let colorPool = [UIColor.systemRed,UIColor.systemGreen,UIColor.systemBlue,UIColor.systemYellow, UIColor.systemPink,UIColor.systemMint]
+    
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        textField.placeholder = "메시지를 입력하세요!"
+        editButton.isHidden = true
+        
     }
-
-
+    
+    @IBAction func editButtonClicked(_ sender: UIButton) {
+        textField.isHidden = false
+        sendButton.isHidden = false
+        randomButton.isHidden = false
+        cycleButton.isHidden = false
+        
+        editButton.isHidden = true
+        //stop color change
+        timeTrigger = true
+        realTime.invalidate()
+        
+       
+    }
+    
+    @IBAction func textFieldEnd(_ sender: Any) {
+        textEntered()
+    }
+    @IBAction func cycleButtonClicked(_ sender: UIButton) {
+        isColorOnRepeat = !isColorOnRepeat
+        if isColorOnRepeat == true{
+            //start
+            print("on")
+            cycleButton.setImage(UIImage(systemName: "repeat.circle.fill"), for: .normal)
+            
+            //trigger color change
+            timeTrigger = true
+            timerAction()
+            
+        }else{
+            //stop
+            print("off")
+            cycleButton.setImage(UIImage(systemName: "repeat.circle"), for: .normal)
+            
+            //stop color change
+            timeTrigger = true
+            realTime.invalidate()
+        }
+                
+        
+    }
+    
+    @IBAction func sendButtonClicked(_ sender: UIButton) {
+        textEntered()
+    }
+    
+    @IBAction func randomButtonClicked(_ sender: UIButton) {
+        let randomColor = colorPool.randomElement()
+        labelView.textColor = randomColor
+        textField.textColor = randomColor
+    }
+    
+    func textEntered(){
+        labelView.text = textField.text
+        view.endEditing(true)
+        
+        //hide buttons
+        textField.isHidden = true
+        sendButton.isHidden = true
+        randomButton.isHidden = true
+        cycleButton.isHidden = true
+        
+        //show edit button
+        editButton.isHidden = false
+        
+      
+        
+    }
+    
+    func timerAction(){
+        if(timeTrigger){
+            checkTimeTrigger()
+        }else{
+            return
+        }
+    }
+    func checkTimeTrigger() {
+        realTime = Timer.scheduledTimer(timeInterval: 0.5, target: self,
+            selector: #selector(changeColor), userInfo: nil, repeats: true)
+    }
+    @objc func changeColor(){
+        print("change color")
+        let randomColor = colorPool.randomElement()
+        labelView.textColor = randomColor
+    }
+    
 }
 
